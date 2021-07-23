f7a97cdb52a3848a9ec04647bb0753649d77cf26
biocrystal777 <benedikt.haeusele@uni-konstanz.de>
2021-07-23 21:55:19 +0200

    Added exercises

--- addStuff.sh
+++ addStuff.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-git add Exercises_ger.pdf Exercises_ger.tex functionOverview.pdf functionOverview.tex subsections imgs solutions addStuff.sh
+git add Exercises_ger.pdf Exercises_ger.tex functionOverview.pdf\
+    functionOverview.tex subsections imgs solutions exerciseCode \
+    addStuff.sh
 
